da8384abc844983f5551d9a08a2d9625a6482c48
pedroruizconekta <alexis.ruiz@conekta.com>
2022-07-13 20:34:06 -0500

    feature(bot): Se agrega separador de mensajes

--- index.js
+++ index.js
@@ -21,16 +21,18 @@ const program = async () => {
     statement: MySQLEvents.STATEMENTS.INSERT,
     onEvent: (event) => {
       const { after: row } = event.affectedRows[0];
+      // Aquí debe ir la url del Webhook por el que discord escuchará los mensajes
       axios
         .post(
-          "https://discord.com/api/webhooks/996178997018906694/eW6G-kDv0fAxaz-Cngx1yH1135VovLS5HTZANm4ciUmh5fZChRULk_YfzsHYz8-m2iLu",
+          "https://discord.com/api/webhooks/996572918601502812/hf6-ibXAtIfCOWqH_V2y7azG_o2YU1HwvbDovfM7u2rBANTabgkK4p_M8c0g9NMtHBa6",
           {
             content:
               `Hay un nuevo reporte con radicado **${row.PK_ID_REPORTE}**\n` +
               `Denunciante: ${row.NOMBREDENUNCIANTE} <@${row.ID_DISCORD_DENUNCIANTE}>\n` +
               `Denunciado: ${row.NOMBREDENUNCIADO} <@${row.ID_DISCORD_DENUNCIADO}>\n` +
               "\n" +
-              `Toda la información del reporte en https://informes.nitrosimracingclub.com/views/formularioapelacion.php?idreporte=${row.PK_ID_REPORTE}&idcalendario=${row.FK_ID_EVENTO}`,
+              `Toda la información del reporte en https://informes.nitrosimracingclub.com/views/formularioapelacion.php?idreporte=${row.PK_ID_REPORTE}&idcalendario=${row.FK_ID_EVENTO}\n` +
+              "------------------------------------------------------------------------------",
           }
         )
         .then((response) => {})
